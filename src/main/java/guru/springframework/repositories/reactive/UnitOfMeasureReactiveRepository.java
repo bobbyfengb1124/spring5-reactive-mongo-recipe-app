da359b64d439525f48d2de7428cc9a65f782e101
Feng Bo <bobbyfeng@cse.com.sg>
2017-10-01 11:36:15 +0800

    added UOM reactive mongo repository

---
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -0,0 +1,14 @@
+/**
+ * author: Feng Bo
+ *
+ * date: Oct 1, 2017
+ */
+package guru.springframework.repositories.reactive;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import guru.springframework.domain.UnitOfMeasure;
+
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+
+}
