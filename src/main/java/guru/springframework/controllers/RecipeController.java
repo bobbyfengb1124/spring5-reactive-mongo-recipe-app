a0b8d83b292e56ff0cdaba759ee9446356ed9e9b
Feng Bo <bobbyfeng@cse.com.sg>
2017-10-17 22:47:43 +0800

    make the controller return reactive type of data

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
@@ -18,6 +19,13 @@ public class RecipeController {
 
     private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     private final RecipeService recipeService;
+    
+    private WebDataBinder webDataBinder;
+    
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+    	this.webDataBinder = webDataBinder;
+    }
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
@@ -26,7 +34,7 @@ public class RecipeController {
     @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
 
-        model.addAttribute("recipe", recipeService.findById(id).block());
+        model.addAttribute("recipe", recipeService.findById(id));
 
         return "recipe/show";
     }
@@ -45,7 +53,9 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+    public String saveOrUpdate(@ModelAttribute("recipe") RecipeCommand command){
+    	webDataBinder.validate();
+    	BindingResult bindingResult = webDataBinder.getBindingResult();
 
         if(bindingResult.hasErrors()){
 
